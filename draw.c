468df4b54611b3f3946d061bf552c440a16a4cfd
Henry Filosa <henryfilosa@henrys-imac.home>
2016-02-21 11:41:07 -0500

    half done

--- draw.c
+++ draw.c
@@ -7,16 +7,17 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  int delta;
   int delta_a=y1-y0;
   int delta_b=-(x1-x0);
   double slope;
   if (delta_b==0)
     slope=2;
   else
-    slope=(delta_a*-1.0)/(delta_b*-1.0);
-  printf("slope: %d\n",slope);
+    slope=(double)(delta_a*1.0)/(double)(delta_b*-1.0);
+  printf("slope: %f delta_a: %d delta_b: %d \n",slope,delta_a,delta_b);
   if (slope>=0 && slope<=1){
-    int delta=2*delta_a+delta_b;
+    delta=(2*delta_a)+delta_b;
     while(x0<=x1){
       plot(s,c,x0,y0);
       if (delta>0){
@@ -28,10 +29,10 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
     }
   }
   if (slope>1){
-    int delta=delta_a+2*delta_b;
+    delta=delta_a+(2*delta_b);
     while(y0<=y1){
       plot(s,c,x0,y0);
-      if (delta>0){
+      if (delta<0){
 	x0+=1;
 	delta+=2*delta_a;
       }
@@ -40,15 +41,27 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
     }
   }
   if (slope<0 && slope>= -1){
-    int delta=2*delta_a+delta_b;
+    delta=2*delta_a-delta_b;
     while(x0<=x1){
       plot(s,c,x0,y0);
-      if (delta>0){
+      if (delta<0){
 	y0-=1;
-	delta+=2*delta_b;
+	delta-=2*delta_b;
       }
       x0+=1;
       delta+=2*delta_a;
+    }
+  }
+  if (slope<-1){
+    delta=delta_a-(2*delta_b);
+    while(y0>=y1){
+      plot(s,c,x0,y0);
+      if (delta>0){
+	x0+=1;
+	delta+=2*delta_a;
+      }
+      y0-=1;
+      delta-=2*delta_b;
     }
   }
 }
